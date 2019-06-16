c8ef6984e396777177d080b25f4d9f633bc64be9
William Hockey <williamrhockey@gmail.com>
2019-06-16 15:30:31 +0300

    kitchen sink

---
+++ index.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('https://visitpalestine.tech/jobs', {waitUntil: 'networkidle2'});
+
+  const result = await page.evaluate(() => {
+    let jobs = [];
+    
+    document.querySelectorAll('.sqs-gallery-design-list-slide')
+      .forEach(elem => {
+        const description = elem.querySelector('.summary-title').innerText;
+
+        jobs.push({
+          title: description.match(/[^\(]*/)[0],
+          summary: title: description.match(/[^\(]*/)[0],
+        })
+      })
+    return jobs;
+  });
+
+  console.log(result)
+  await browser.close();
+})();
