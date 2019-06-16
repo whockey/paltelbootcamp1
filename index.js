b23b81c408d92719560585cc71386d84a722b0bd
webproject <anahek.smy.91@gmail.com>
2019-06-16 16:03:08 +0300

    hit is True

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
 
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
 
   const page = await browser.newPage();
